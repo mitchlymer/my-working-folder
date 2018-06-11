5f97ccfbfd0de80e936172d755838b386e7a84a6
MitchL <mitchlymer@gmail.com>
2018-06-11 15:41:33 -0400

    brainstation first commit

---
+++ Brainstation/assignment-1/assignment-1.js
@@ -0,0 +1,22 @@
+// Create a variable named `numbers` and assign an empty array to it.
+// Using a for-loop and the Array push() method, insert the integers (numbers) 0 to 9 into the array you named numbers.
+// Test that you used the array push method correctly by console logging the first item in the array.
+// Console log the last item in the array.
+// Create another variable named car and assign an empty object to it.
+// Give the object a property called colour and assign it the value of "blank".
+
+var numbers = [];
+numbers.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+
+
+
+for (var i=0; i < 10; i++){
+    
+}
+
+console.log(numbers[0]);
+console.log(numbers[9]);
+
+var car = new Object();
+
+car.colour = ();
---
+++ Brainstation/assignment-2/assignment-2.js
@@ -0,0 +1,10 @@
+// Create a JavaScript file and name it assignment-2.js. Inside this file, write JavaScript code solving the two challenges below. Also include an explanation of what your code is doing in your own words.
+// You can write plain English in JavaScript files using multi-line comments.
+
+
+//Write a loop that logs the following triangle to the console:
+//Diving Deeper: Are you using `console.log()` multiple times for each line? Can you do it with one call for all 7 lines of hashes?
+
+
+// Write a function that takes in an integer (number) as a parameter. The function must log `"the number is even"` if the given parameter is an even number, and log `"the number is odd"` if it is odd.
+// Diving Deeper: What happens if the given parameter is not a number at all?
